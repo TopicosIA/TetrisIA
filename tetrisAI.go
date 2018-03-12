82102e281ee3192a0412e43015f192e5dc1868d9
ErickLF <y3rick_18@hotmail.com>
2018-03-12 16:45:22 -0700

    desde este archivo se manda a llamar todo el juego

---
+++ tetrisAI.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+
+	//"math/rand"
+	"os"
+	"path/filepath"
+	"gopkg.in/inconshreveable/log15.v2"
+)
+
+func main() {
+
+	baseDir, _ = filepath.Abs(filepath.Dir(os.Args[0]))
+	logger = log15.New()
+	if baseDir != "" {
+		logger.SetHandler(log15.Must.FileHandler(baseDir+"/tetrisAI.log", log15.LogfmtFormat()))
+	}
+
+	//rand.Seed(time.Now().UnixNano())
+	CrearFiguras()
+	NuevoTablero()
+	NuevaInterfaz()
+	NuevoMotorJuego()
+
+	motor.Run()
+	interfaz.Stop()
+
+}
