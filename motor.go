3cbc6276406e5afac55ead448c8e53c70643cb8c
ErickLF <y3rick_18@hotmail.com>
2018-03-19 00:39:43 -0700

    Ya funciona la logica del juego

--- motor.go
+++ motor.go
@@ -14,7 +14,7 @@ func NuevoMotorJuego() {
 	}
 }
 func (motor *MotorJuego) Run() {
-  logger.Info("Engine Run start")
+  logger.Info("MotorJuego Run start")
 
 	var event *termbox.Event
 
@@ -23,6 +23,7 @@ func (motor *MotorJuego) Run() {
 	motor.timer.Stop()
 
 	//preparamos el juego
+  motor.ranking = NewRanking()
   tablero.Limpiar()
 	interfaz.refrescarPantalla()
 
@@ -40,14 +41,14 @@ func (motor *MotorJuego) Run() {
   			case event = <-motor.tecla.opciones_teclas:
   				motor.tecla.ProcesarEvento(event)
   				interfaz.refrescarPantalla()
-  			//case <-motor.timer.C:
-  			//	motor.tick()
+  			case <-motor.timer.C:
+  				motor.tick() //desplazamos la pieza hacia abajo
       case <-motor.chanStop:
   				break loop
   			}
   		}
   	}
-    logger.Info("Engine Run end")
+    logger.Info("MotorJuego Run end")
 }
 
 func (motor *MotorJuego) PreviewBoard() {
@@ -55,25 +56,25 @@ func (motor *MotorJuego) PreviewBoard() {
 }
 
 func (motor *MotorJuego) Stop() {
-	logger.Info("Engine Stop start")
+	logger.Info("MotorJuego Stop start")
 
 	if !motor.stopped {
 		motor.stopped = true
 		close(motor.chanStop)
 	}
-	//motor.timer.Stop()
+	motor.timer.Stop()
 	//motor.aiTimer.Stop()
 
-	logger.Info("Engine Stop end")
+	logger.Info("MotorJuego Stop end")
 }
 func (motor *MotorJuego) NuevoJuego() {
-	logger.Info("Engine NewGame start")
+	logger.Info("MotorJuego NewGame start")
 
 	//iniciamos el nivel
 	tablero.Limpiar()
 	motor.tableroInicio = false
 	motor.gameOver = false
-	/*motor.tickTime = 480 * time.Millisecond
+	motor.tickTime = 480 * time.Millisecond
 	motor.score = 0
 	motor.nivel = 1
 	motor.lineasBorradas = 0
@@ -86,20 +87,17 @@ loop:
 			break loop
 		}
 	}
-
+//hace que la pieza se desplace
 	motor.QuitarPausa()
-*/
-	logger.Info("Engine NewGame end")
+
+	logger.Info("MotorJuego NewGame end")
 }
 
 func (motor *MotorJuego) GameOver() {
-	logger.Info("Engine GameOver start")
-
+	logger.Info("MotorJuego GameOver start")
 	motor.Pause()
 	motor.gameOver = true
-
-	//view.ShowGameOverAnimation()
-
+  interfaz.MostrarAnimacionGameOver()
 loop:
 	for {
 		select {
@@ -109,10 +107,10 @@ loop:
 		}
 	}
 
-	//engine.ranking.InsertScore(uint64(engine.score))
-	//engine.ranking.Save()
+	motor.ranking.InsertScore(uint64(motor.score))
+	motor.ranking.Save()
 
-	logger.Info("Engine GameOver end")
+	logger.Info("MotorJuego GameOver end")
 }
 
 func (motor *MotorJuego) Pause() {
@@ -129,6 +127,7 @@ func (motor *MotorJuego) QuitarPausa(){
   motor.timer.Reset(motor.tickTime)
 	motor.pausado = false
 }
+
 //hace bajar la figura cada cierto tiempo
 func (motor *MotorJuego) tick() {
 	tablero.MoverFiguraAbajo()
@@ -148,5 +147,56 @@ func (motor *MotorJuego) ResetTimer(duracion time.Duration) {
 	} else {
 		// duration is lock delay
 		motor.timer.Reset(duracion)
+	}
+}
+
+func (motor *MotorJuego) AGregarLineasBorradas(lines int) {
+	motor.lineasBorradas += lines
+	if motor.lineasBorradas > 999999 {
+		motor.lineasBorradas = 999999
+	}
+
+	switch lines {
+	case 1:
+		motor.SumaPts(40 * (motor.nivel + 1))
+	case 2:
+		motor.SumaPts(100 * (motor.nivel + 1))
+	case 3:
+		motor.SumaPts(300 * (motor.nivel + 1))
+	case 4:
+		motor.SumaPts(1200 * (motor.nivel + 1))
+	}
+
+	if motor.nivel < motor.lineasBorradas/10 {
+		motor.LevelUp()
+	}
+}
+
+func (motor *MotorJuego) SumaPts(pts int) {
+	motor.score += pts
+	if motor.score > 9999999 {
+		motor.score = 9999999
+	}
+}
+func (motor *MotorJuego) LevelUp() {
+	if motor.nivel >= 30 {
+		return
+	}
+
+	motor.nivel++
+	switch {
+	case motor.nivel > 29:
+		motor.tickTime = 10 * time.Millisecond
+	case motor.nivel > 25:
+		motor.tickTime = 20 * time.Millisecond
+	case motor.nivel > 19:
+		// 50 to 30
+		motor.tickTime = time.Duration(10*(15-motor.nivel/2)) * time.Millisecond
+	case motor.nivel > 9:
+		// 150 to 60
+		motor.tickTime = time.Duration(10*(25-motor.nivel)) * time.Millisecond
+	default:
+		// 480 to 160
+		motor.tickTime = time.Duration(10*(52-4*motor.nivel)) * time.Millisecond
 	}
 }
